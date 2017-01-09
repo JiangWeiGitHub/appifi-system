7ce9dc18e2b459143176a8955ba1a152774d1746
John <wei.jiang@winsuntech.cn>
2017-01-09 18:04:45 +0800

    Update install-appifi.sh

--- wisnuc-215i/ubuntu-16-04-1-amd64/install-appifi.sh
+++ wisnuc-215i/ubuntu-16-04-1-amd64/install-appifi.sh
@@ -195,8 +195,8 @@ systemctl stop smbd nmbd
 systemctl disable smbd nmbd
 
 # just for test
-wget https://github.com/JiangWeiGitHub/appifi-system/raw/master/wisnuc-215i/ubuntu-16-04-1-amd64/appifi-0.2.16-4895487-3a2df4d1-pre.tar.gz
-mv appifi-0.2.16-4895487-3a2df4d1-pre.tar.gz /wisnuc/appifi-tarballs
+https://github.com/JiangWeiGitHub/appifi-system/raw/master/wisnuc-215i/ubuntu-16-04-1-amd64/appifi-0.2.20-5101484-dbffb437-pre.tar.gz
+mv appifi-0.2.20-5101484-dbffb437-pre.tar.gz /wisnuc/appifi-tarballs
 
 #
 # cleanup
